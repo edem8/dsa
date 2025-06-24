bf7f532e68983d8f7dbccb8a3eda0d9346e6ad06
edem8 <eddieblack159@gmail.com>
2025-06-24 00:56:58 +0000

    chore: updated Node list grpah to Adjacent list Graph

--- data-structures/graphs.py
+++ data-structures/graphs.py
@@ -1,34 +1,21 @@
-# Graphs have like nodes and edges which establishes some realtion between nodes
-# so like some node is connected to another node
 
-
-class Node:
-    def __init__(self, id):
-        self.id = id
-        # nodes are connected to other nodes - list of nodes
-        self.neighbours = []
 
 
 class Graph:
     def __init__(self):
-        self.nodes = []
+        self.nodes = {}  # id → set of neighbor ids
 
-    def add_node(self, node):
-        self.nodes.append(node)
+    def add_edge(self, from_id, to_id):
+        # Initialize sets if not present
+        # And add two undirected edges
+        self.nodes.setdefault(from_id, set()).add(to_id)
+        self.nodes.setdefault(to_id, set()).add(from_id)  # undirected graph
 
-    def add_edge(self, id_1, id_2):
-        node1 = node2 = None
+    def __repr__(self):
+        return "\n".join(f"{node}: {neighs}" for node, neighs in self.nodes.items())
 
-        # Find nodes by their IDs
-        for node in self.nodes:
-            if node.id == id_1:
-                node1 = node
-            elif node.id == id_2:
-                node2 = node
 
-        if node1 and node2:
-            node1.neighbours.append(node2)
-            node2.neighbours.append(node1)
+
 
 
 #  In a real world application like a social application you have more dense relations and
